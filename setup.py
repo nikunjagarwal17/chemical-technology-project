716734ff9ba8612393654ed577f20dabbdefa19e
Nikunj Agarwal <nikunjagarwal1704@outlook.com>
2025-08-19 17:42:42 +0530

    fix5

--- setup.py
+++ setup.py
@@ -3,47 +3,60 @@ import os
 
 install_requires = ['numpy']
 
-# Try to build Cython extension if Cython is available
-extensions = []
+# Build the compiled extension. Prefer a pre-generated C++ file so builds in
+# isolated build environments (cibuildwheel containers) don't require Cython
+# to be present at build time. If the generated C++ doesn't exist, fall back
+# to using Cython to cythonize the .pyx file.
+include_dirs = [os.path.abspath('src')]
 try:
-    from Cython.Build import cythonize
-    # include numpy headers when available (required for Cython wrappers that use numpy)
-    include_dirs = [os.path.abspath('src')]
-    try:
-        import numpy
-        include_dirs.append(numpy.get_include())
-    except Exception:
-        # numpy headers not available at setup-time; build may still work if numpy is
-        # installed in the environment used by pip. If build fails with missing
-        # 'numpy/arrayobject.h', install numpy in the build environment and retry.
-        pass
+    import numpy
+    include_dirs.append(numpy.get_include())
+except Exception:
+    # numpy headers may be provided in the build environment; continue.
+    pass
 
-    # Try to discover Python's libs dir and release import lib name so the linker
-    # can find the correct pythonXXX.lib even if a debug import lib (python313t.lib)
-    # is not present. This avoids requiring admin to copy files in the Python install.
-    try:
-        import sysconfig, sys
-        libdir = sysconfig.get_config_var('LIBDIR') or os.path.join(sys.exec_prefix, 'libs')
-        py_lib_name = f'python{sys.version_info.major}{sys.version_info.minor}'
-    except Exception:
-        libdir = os.path.join(sys.exec_prefix, 'libs')
-        py_lib_name = None
+# Try to discover Python's libs dir and release import lib name to help the linker
+try:
+    import sysconfig, sys
+    libdir = sysconfig.get_config_var('LIBDIR') or os.path.join(sys.exec_prefix, 'libs')
+    py_lib_name = f'python{sys.version_info.major}{sys.version_info.minor}'
+except Exception:
+    libdir = os.path.join(sys.exec_prefix, 'libs')
+    py_lib_name = None
 
-    # prefer project root for library lookup so we can provide a local shim if
-    # the Python install doesn't expose a debug import lib (pythonXXXt.lib)
-    library_dirs = [os.path.abspath('.')] + ([libdir] if libdir else [])
+library_dirs = [os.path.abspath('.')] + ([libdir] if libdir else [])
+
+sources = []
+if os.path.exists(os.path.join('simplecantera', 'pybindings.cpp')):
+    # Use the generated C++ file
+    sources = ['simplecantera/pybindings.cpp', 'src/core.cpp']
     ext = Extension(
         'simplecantera._pybindings',
-        sources=['simplecantera/pybindings.pyx', 'src/core.cpp'],
+        sources=sources,
         language='c++',
-        include_dirs=include_dirs,
+        include_dirs=include_dirs + [os.path.abspath('simplecantera')],
         library_dirs=library_dirs,
         libraries=[py_lib_name] if py_lib_name else [],
     )
-    extensions = cythonize([ext])
-except Exception:
-    # Cython not available; skip building extension
-    extensions = []
+    extensions = [ext]
+else:
+    # Fall back to Cython -> .pyx
+    try:
+        from Cython.Build import cythonize
+        sources = ['simplecantera/pybindings.pyx', 'src/core.cpp']
+        ext = Extension(
+            'simplecantera._pybindings',
+            sources=sources,
+            language='c++',
+            include_dirs=include_dirs + [os.path.abspath('simplecantera')],
+            library_dirs=library_dirs,
+            libraries=[py_lib_name] if py_lib_name else [],
+        )
+        extensions = cythonize([ext])
+    except Exception:
+        # If Cython isn't available and no generated C++ exists, don't build
+        # a compiled extension (fall back to pure-Python package).
+        extensions = []
 
 setup(
     name='simplecantera',
